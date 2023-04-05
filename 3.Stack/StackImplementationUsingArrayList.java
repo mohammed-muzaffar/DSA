54825b04c9c7bab068c74cb4fa6cf77247485f0c
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-04-06 01:16:49 +0530

    StackImplementationUsingArrayList.java

---
+++ 3.Stack/StackImplementationUsingArrayList.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class StackImplementationUsingArrayList {
+
+    static class Stack {
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty() {
+            return (list.isEmpty());
+        }
+
+        // push
+        public static void push(int e) {
+            list.add(e);
+        }
+
+        // pop
+        public static int pop() {
+            if (list.size() == 0) {
+                return -1;
+            }
+
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        // peek
+
+        public static int peek() {
+            if (list.size() == 0) {
+                return -1;
+            }
+            return (list.get(list.size() - 1));
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Stack.push(1);
+        Stack.push(2);
+        Stack.push(3);
+
+        while (!Stack.isEmpty()) {
+            System.out.println(Stack.peek());
+            Stack.pop();
+        }
+    }
+}
