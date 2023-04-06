bb58ad4e5a894eedc146912a860c87d3ccaea2fe
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-04-06 20:25:09 +0530

    ReverseString

---
+++ 3.Stack/ReverseString.java
@@ -0,0 +1,18 @@
+
+//Reverse String
+import java.util.Stack;
+
+class Solution {
+    public void reverseString(char[] s) {
+        Stack<Character> stack = new Stack<>();
+        int n = s.length;
+
+        for (int i = 0; i < n; i++) {
+            stack.push(s[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            s[i] = stack.pop();
+        }
+    }
+}
