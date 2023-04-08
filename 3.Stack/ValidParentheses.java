a83347f03349ecb44c80f6abc6fcb7de9422bd7d
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-04-09 00:49:16 +0530

    Valid Parentheses

---
+++ 3.Stack/ValidParentheses.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+import javax.swing.text.html.HTMLDocument.RunElement;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public static boolean valid(String str) {
+        int n = str.length();
+        Stack<Character> s = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            char x = str.charAt(i);
+            if (x == '[' || x == '{' || x == '(') {
+                s.push(str.charAt(i));
+            } else {
+                char c = str.charAt(i);
+                char check;
+                switch (c) {
+                    case '}':
+                        check = s.pop();
+                        if (check == ']' || check == ')') {
+                            return false;
+                        }
+                        break;
+
+                    case ']':
+                        check = s.pop();
+                        if (check == ')' || check == '}') {
+                            return false;
+                        }
+                        break;
+
+                    case ')':
+                        check = s.pop();
+                        if (check == '}' || check == ')') {
+                            return false;
+                        }
+                        break;
+
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "(]";
+        System.out.println(ValidParentheses.valid(str));
+    }
+}
