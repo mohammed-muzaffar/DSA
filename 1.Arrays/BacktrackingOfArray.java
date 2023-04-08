5d2508f230988a5bb4bdcff4543e192332897bee
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-04-09 01:36:04 +0530

    Modification in stack and array

--- 1.Arrays/BacktrackingOfArray.java
+++ 1.Arrays/BacktrackingOfArray.java
@@ -1,30 +1,30 @@
-public class BackTrackingArr{
-    
-    public void changeArr(int[] arr, int i, int val){
-        if(i == arr.length){
+public class BacktrackingOfArray {
+
+    public static void changeArr(int[] arr, int i, int val) {
+        if (i == arr.length) {
             printArr(arr);
             return;
         }
 
-        arr[i] =  val;
-        change(arr, i+1, val+1);
+        arr[i] = val;
+        changeArr(arr, i + 1, val + 1);
         arr[i] = arr[i] - 2;
     }
 
-    public void printArr(int[] arr){
-        for(int i=0; i<n; i++){
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
 
         }
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         int[] arr = new int[5];
         changeArr(arr, 0, 1);
         printArr(arr);
     }
-    
+
 }
 
-//Time Complexity O(n)
-//Space Complexity o(n) stack Overflow
+// Time Complexity O(n)
+// Space Complexity o(n) stack Overflow
