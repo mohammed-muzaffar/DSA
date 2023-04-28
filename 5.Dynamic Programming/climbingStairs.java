fba2ac8d8d6ffcf8dfb1e83ff557d7eb5295858a
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-04-29 00:29:08 +0530

    DP approach

--- 5.Dynamic Programming/climbingStairs.java
+++ 5.Dynamic Programming/climbingStairs.java
@@ -6,7 +6,15 @@ public class climbingStairs {
         if (n == 1)
             return 1;
 
-        return f(n - 1) + f(n - 2);
+        int[] dp = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n - 1];
     }
 
     public static void main(String args[]) {
