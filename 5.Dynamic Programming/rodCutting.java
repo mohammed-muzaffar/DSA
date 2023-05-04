0890ea850073d27dd9f47ab5307d353aefcb12ac
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-05-04 21:12:38 +0530

    rodCutting DP

--- 5.Dynamic Programming/rodCutting.java
+++ 5.Dynamic Programming/rodCutting.java
@@ -1,24 +1,24 @@
+import java.util.Arrays;
+
 class rodCutting {
 
-    static int rodCuttingDP(int[] price, int n) {
-        if (n == 0)
-            return 0;
-
-        if (n < 0)
-            return -10000;
-
-        int max = 0;
-
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, price[i] + rodCuttingDP(price, n - i - 1));
+    static int rodCuttingDP(int[] price, int n, int[] dp) {
+        if (dp[n] != -1) {
+            return dp[n];
         }
-        return max;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, price[i] + rodCuttingDP(price, n - i - 1, dp));
+        }
+        return dp[n] = max;
     }
 
     public static void main(String args[]) {
         int[] price = { 1, 5, 8, 9, 10, 17, 17, 20 };
-
         int n = 8;
-        System.out.println(rodCuttingDP(price, n));
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+
+        System.out.println(rodCuttingDP(price, n, dp));
     }
 }
