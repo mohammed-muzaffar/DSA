fd52afc736ef7d10d2f84e773268f7328d1534db
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-05-05 22:23:49 +0530

    longest Sequence DP

--- 5.Dynamic Programming/longestSubsequence.java
+++ 5.Dynamic Programming/longestSubsequence.java
@@ -1,25 +1,37 @@
+import java.util.Arrays;
+
 public class longestSubsequence {
 
-    static int longestCommonSubsequence(String str1, String str2, int n, int m) {
-
-        if (n == 0 || m == 0) {
+    public static int lcs(String text1, String text2, int m, int n, int[][] dp) {
+        if (m == 0 || n == 0)
             return 0;
-        }
+        if (dp[m - 1][n - 1] != -1)
+            return dp[m][n];
 
-        if (str1.charAt(n - 1) == str2.charAt(m - 1)) {
-            return 1 + longestCommonSubsequence(str1, str2, n - 1, m - 1);
+        if (text1.charAt(m - 1) == text2.charAt(n - 1)) {
+            return dp[m - 1][n - 1] = 1 + lcs(text1, text2, m - 1, n - 1, dp);
         } else {
-            return Math.max(longestCommonSubsequence(str1, str2, n - 1, m),
-                    longestCommonSubsequence(str1, str2, n, m - 1));
+            return dp[m - 1][n - 1] = Math.max(lcs(text1, text2, m - 1, n, dp), lcs(text1, text2, m, n - 1, dp));
         }
     }
 
-    public static void main(String args[]) {
-        String str1 = "AGGTAB";
-        String str2 = "GXTXAYB";
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
 
-        int n = str1.length();
-        int m = str2.length();
-        System.out.println(longestCommonSubsequence(str1, str2, n, m));
+        int dp[][] = new int[m][n];
+
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return lcs(text1, text2, m, n, dp);
+    }
+
+    public static void main(String args[]) {
+        String str1 = "abc";
+        String str2 = "def";
+
+        System.out.println(longestCommonSubsequence(str1, str2));
     }
 }
