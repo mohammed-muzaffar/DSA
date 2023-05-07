6b0062876266cc79150a81a4c893619596f65664
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-05-07 16:44:58 +0530

    Recursion Folder

---
+++ 6.Recursion/Subsequences.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+class Subsequences {
+
+    static void solve(int[] arr, int ind, ArrayList<Integer> list) {
+        if (ind >= arr.length) {
+            for (int e : list) {
+                System.out.print(e + " ");
+            }
+            if (list.size() == 0) {
+                System.out.print("{}");
+            }
+            System.out.println();
+            return;
+        }
+
+        solve(arr, ind + 1, list);
+        list.add(arr[ind]);
+        solve(arr, ind + 1, list);
+        list.remove(list.size() - 1);
+    }
+
+    public static void main(String args[]) {
+        int[] arr = { 3, 1, 2 };
+        int n = arr.length;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        solve(arr, 0, list);
+    }
+}
