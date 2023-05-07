610b2924de629ec06c300632cdc486a695ec70c5
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-05-08 01:51:55 +0530

    NinjaTrainingDP

--- 5.Dynamic Programming/NinjaTraining.java
+++ 5.Dynamic Programming/NinjaTraining.java
@@ -1,8 +1,8 @@
+import java.util.Arrays;
 
 class NinjaTraining {
 
-    static int solve(int[][] points, int day, int last) {
-
+    static int solve(int[][] points, int day, int last, int[][] dp) {
         if (day == 0) {
             int maxi = Integer.MIN_VALUE;
             for (int task = 0; task < 3; task++) {
@@ -12,25 +12,29 @@ class NinjaTraining {
             }
             return maxi;
         }
-        int maxi = Integer.MIN_VALUE;
 
+        if (dp[day][last] != -1)
+            return dp[day][last];
+
+        int maxi = Integer.MIN_VALUE;
         for (int task = 0; task < 3; task++) {
             if (task != last) {
-                int activites = points[day][task] + solve(points, day - 1, task);
+                int activites = points[day][task] + solve(points, day - 1, task, dp);
                 maxi = Math.max(maxi, activites);
             }
-
         }
-        return maxi;
-
+        return dp[day][last] = maxi;
     }
 
     public static void main(String args[]) {
         int[][] points = { { 10, 40, 70 },
                 { 20, 50, 80 },
                 { 30, 60, 90 } };
-
         int n = points.length;
-        System.out.println(solve(points, n - 1, 3));
+        int[][] dp = new int[n][4];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        System.out.println(solve(points, n - 1, 3, dp));
     }
 }
