8758c047b12422e6756d88cff244eae27242f823
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-05-07 16:52:21 +0530

    space optimization

--- 5.Dynamic Programming/longestSubsequence.java
+++ 5.Dynamic Programming/longestSubsequence.java
@@ -2,6 +2,27 @@ import java.util.Arrays;
 
 public class longestSubsequence {
 
+    // tabulation format
+    public static int lcsTabular(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int[][] dp = new int[m][n];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; i <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+
+    }
+
+    // memoization
     public static int lcs(String text1, String text2, int m, int n, int[][] dp) {
         if (m == 0 || n == 0)
             return 0;
@@ -13,6 +34,7 @@ public class longestSubsequence {
         } else {
             return dp[m - 1][n - 1] = Math.max(lcs(text1, text2, m - 1, n, dp), lcs(text1, text2, m, n - 1, dp));
         }
+
     }
 
     public static int longestCommonSubsequence(String text1, String text2) {
@@ -25,13 +47,22 @@ public class longestSubsequence {
             Arrays.fill(row, -1);
         }
 
-        return lcs(text1, text2, m, n, dp);
+        int ans = lcs(text1, text2, m, n, dp);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(dp[i][j] + " ");
+            }
+            System.out.println();
+        }
+        return ans;
     }
 
     public static void main(String args[]) {
-        String str1 = "abc";
-        String str2 = "def";
+        String str1 = "abcde";
+        String str2 = "ace";
 
-        System.out.println(longestCommonSubsequence(str1, str2));
+        System.out.println(lcsTabular(str1, str2));
+
     }
 }
