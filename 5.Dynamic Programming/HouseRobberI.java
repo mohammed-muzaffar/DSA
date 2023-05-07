bee21e082b5083baa05ce873505185cbd972cbe5
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-05-07 22:05:00 +0530

    HouseRobberI DP

--- 5.Dynamic Programming/HouseRobberI.java
+++ 5.Dynamic Programming/HouseRobberI.java
@@ -1,23 +1,30 @@
+import java.util.Arrays;
+
 public class HouseRobberI {
 
-    static int solve(int[] nums, int ind) {
+    static int solve(int[] nums, int ind, int[] dp) {
         if (ind == 0)
             return nums[ind];
 
         if (ind < 0)
             return 0;
 
-        int pick = nums[ind] + solve(nums, ind - 2);
-        int notPick = 0 + solve(nums, ind - 1);
+        if (dp[ind] != -1)
+            return dp[ind];
 
-        return Math.max(pick, notPick);
+        int pick = nums[ind] + solve(nums, ind - 2, dp);
+        int notPick = 0 + solve(nums, ind - 1, dp);
+
+        return dp[ind] = Math.max(pick, notPick);
     }
 
     public static void main(String args[]) {
 
         int[] nums = { 2, 1, 4, 9 };
         int n = nums.length;
-        System.out.println(solve(nums, n - 1));
-    
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        System.out.println(solve(nums, n - 1, dp));
+
     }
 }
