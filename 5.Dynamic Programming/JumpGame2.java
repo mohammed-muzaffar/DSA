60b91ddd4a735b67434811da7e5156994e03c2ef
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-05-11 18:59:59 +0530

    JumpGameDP

--- 5.Dynamic Programming/JumpGame2.java
+++ 5.Dynamic Programming/JumpGame2.java
@@ -1,20 +1,27 @@
+import java.util.Arrays;
+
 public class JumpGame2 {
 
-    public static int jumpGame(int[] nums, int idx) {
+    public static int jumpGame(int[] nums, int idx, int[] dp) {
 
         if (idx == nums.length - 1)
-            return 0;
+            return dp[idx] = 0;
 
+        if (dp[idx] != -1)
+            return dp[idx];
         int min = Integer.MAX_VALUE;
         for (int j = idx + 1; j < nums.length && j <= idx + nums[idx]; j++) {
-            int jumps = 1 + jumpGame(nums, j);
+            int jumps = 1 + jumpGame(nums, j, dp);
             min = Math.min(min, jumps);
         }
-        return min;
+        return dp[idx] = min;
+
     }
 
     public static void main(String args[]) {
         int[] nums = { 2, 3, 1, 1, 4 };
-        System.out.println(jumpGame(nums, 0));
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        System.out.println(jumpGame(nums, 0, dp));
     }
 }
