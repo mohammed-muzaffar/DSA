bfaa18e44fca080cf7cbd0d5c51abdbf78c6c005
Mohammed Muzaffar <mohammedmuzaffar126@gmail.com>
2023-05-14 20:29:36 +0530

    Minimum Path Sum Tabulation

--- 5.Dynamic Programming/MinimumPathSum.java
+++ 5.Dynamic Programming/MinimumPathSum.java
@@ -2,36 +2,36 @@ import java.util.Arrays;
 
 public class MinimumPathSum {
 
-    public static int minSum(int[][] grid, int m, int n, int[][] dp) {
-
-        if (m == 0 && n == 0)
-            return grid[0][0];
-
-        if (m < 0 || n < 0)
-            return (int) Math.pow(10, 9);
-        ;
-
-        if (dp[m][n] != -1)
-            return dp[m][n];
-
-        int up = grid[m][n] + minSum(grid, m - 1, n, dp);
-        int left = grid[m][n] + minSum(grid, m, n - 1, dp);
-
-        return dp[m][n] = Math.min(up, left);
-
-    }
-
     public static int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+
         int[][] dp = new int[m][n];
 
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else {
+                    int up = grid[i][j];
+                    if (i > 0) {
+                        up += dp[i - 1][j];
+                    } else {
+                        up += (int) Math.pow(10, 9);
+                    }
+                    int left = grid[i][j];
+                    if (j > 0) {
+                        left += dp[i][j - 1];
+                    } else {
+                        left += (int) Math.pow(10, 9);
+                    }
+                    dp[i][j] = Math.min(up, left);
+
+                }
+            }
         }
 
-        return minSum(grid, m - 1, n - 1, dp);
-
+        return dp[m - 1][n - 1];
     }
 
     public static void main(String args[]) {
